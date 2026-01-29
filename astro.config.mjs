9330c0160b17e477f1fb59c965779cb2f30b383e
Rens Schuil <rensschuil02@users.noreply.github.com>
2026-01-29 16:07:46 +0100

    Add Tailwind CSS configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rensschuil02.github.io',
   base: '/cyber-portfolio',
+  integrations: [tailwind()],
 });
