26738203d59b06d8ed175bd33318b47c9fb2f487
Rens Schuil <rensschuil02@users.noreply.github.com>
2026-01-29 16:09:39 +0100

    Add MDX integration for markdown support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rensschuil02.github.io',
   base: '/cyber-portfolio',
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
 });
