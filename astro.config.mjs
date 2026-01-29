7286bc70911c97cd0959573a9614531c4025b84d
Rens Schuil <rensschuil02@users.noreply.github.com>
2026-01-29 16:05:32 +0100

    Update base path for cyber-portfolio repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rensschuil02.github.io',
-  // Uncomment the line below if deploying to a repository named something other than <username>.github.io
-  // base: '/portfolio-website-project',
+  base: '/cyber-portfolio',
 });
